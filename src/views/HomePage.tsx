b7c4f795510fc6fbb2318c2a4348508509cc178f
shafferma <70526699+shafferma@users.noreply.github.com>
2020-11-21 15:58:24 -0500

    .

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Form, FormGroup, Label, Input, Button } from "reactstrap";
 import "styles/HomePage.css"
+const photo = require('styles/assets/company-tree.JPG')
 
 const HomePage = () => {
+
+  // const [str, setStr] = useState('')
+
+  // const canvas = document.createElement('canvas')
+  // const context = canvas.getContext('2d')
+  // const img = new Image()
+
+  // img.onload = function() {
+
+  //   canvas.height = img.height
+  //   canvas.width = img.width
+    
+  //   context.drawImage(img, 0, 0)
+  //   const str = canvas.toDataURL('image/JPG', '')
+  //   console.info({str})
+  //   setStr(str)
+  // }
+
+  // img.src = photo
+
   return (
     <Container>
       <div id="main">
@@ -11,7 +32,7 @@ const HomePage = () => {
       </div>
       <div>
         {/* photo grid display */}
-      
+        {'sefsfsef'}
       </div>
     </Container>
   );
